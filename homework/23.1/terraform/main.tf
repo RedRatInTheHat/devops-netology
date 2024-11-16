e77e85644968bc92a650f795471f8149fd31620a
Pesheva Irina <pesheva.i@yandex.ru>
2024-11-16 14:20:03 +0300

    Add solution for 23.1.1.2

--- homework/23.1/terraform/main.tf
+++ homework/23.1/terraform/main.tf
@@ -1,3 +1,114 @@
+# VPC
+
 resource "yandex_vpc_network" "vpc" {
   name = var.vpc_name
 }
+
+resource "yandex_vpc_subnet" "public" {
+  name           = var.subnet_default_name
+  zone           = var.default_zone
+  network_id     = yandex_vpc_network.vpc.id
+  v4_cidr_blocks = var.default_cidr
+}
+
+# NAT
+
+## Boot Disks
+
+resource "yandex_compute_disk" "boot-disk-nat" {
+  name     = var.nat_boot_disk_name
+  type     = var.boot_disk_type
+  zone     = var.default_zone
+  size     = var.boot_disk_size
+  image_id = var.nat_boot_disk_image_id
+}
+
+resource "yandex_compute_disk" "boot-disk-public-vm" {
+  name     = var.vm_boot_disk_name
+  type     = var.boot_disk_type
+  zone     = var.default_zone
+  size     = var.boot_disk_size
+  image_id = var.vm_boot_disk_image_id
+}
+
+## Security Group
+
+resource "yandex_vpc_security_group" "nat-instance-sg" {
+  name       = var.sg_nat_name
+  network_id = yandex_vpc_network.vpc.id
+
+  egress {
+    protocol       = "ANY"
+    description    = "any"
+    v4_cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    protocol       = "TCP"
+    description    = "ssh"
+    v4_cidr_blocks = ["0.0.0.0/0"]
+    port           = 22
+  }
+}
+
+
+## VM
+
+resource "yandex_compute_instance" "nat-instance" {
+  name        = var.vm_nat_name
+  platform_id = var.platform_id
+  zone        = var.default_zone
+
+  allow_stopping_for_update = var.allow_stopping_for_update
+
+  resources {
+    core_fraction = var.core_fraction
+    cores         = var.cores
+    memory        = var.memory
+  }
+
+  boot_disk {
+    disk_id = yandex_compute_disk.boot-disk-nat.id
+  }
+
+  network_interface {
+    subnet_id          = yandex_vpc_subnet.public.id
+    security_group_ids = [yandex_vpc_security_group.nat-instance-sg.id]
+    nat                = true
+    ip_address         = var.nat_ip_address
+  }
+
+  metadata = {
+    user-data = "${file("${var.metadata-path}")}"
+  }
+}
+
+# Public VM
+resource "yandex_compute_instance" "public-vm" {
+  name        = var.public_vm_name
+  platform_id = var.platform_id
+  zone        = var.default_zone
+
+  allow_stopping_for_update = var.allow_stopping_for_update
+
+  resources {
+    core_fraction = var.core_fraction
+    cores         = var.cores
+    memory        = var.memory
+  }
+
+  boot_disk {
+    disk_id = yandex_compute_disk.boot-disk-public-vm.id
+  }
+
+  network_interface {
+    subnet_id          = yandex_vpc_subnet.public.id
+    nat                = true
+  }
+
+  metadata = {
+    user-data = "${file("${var.metadata-path}")}"
+  }
+}
+
+
--- homework/23.1/terraform/variables.tf
+++ homework/23.1/terraform/variables.tf
@@ -1,4 +1,4 @@
-## Cloud vars
+# Cloud vars
 variable "cloud_id" {
   type        = string
   description = "https://cloud.yandex.ru/docs/resource-manager/operations/cloud/get-id"
@@ -15,9 +15,130 @@ variable "default_zone" {
   description = "https://cloud.yandex.ru/docs/overview/concepts/geo-scope"
 }
 
-## VPC
+# VPC
 
 variable "vpc_name" {
     type = string
     default = "vpc"
+}
+
+variable "subnet_default_name" {
+    type        = string
+    default     = "public"
+    description = "Default subnet name"
+}
+
+variable "default_cidr" {
+  type        = list(string)
+  default     = ["192.168.10.0/24"]
+  description = "https://cloud.yandex.ru/docs/vpc/operations/subnet-create"
+}
+
+# VM
+
+variable "platform_id" {
+  description = "Platform for the virtual machine"
+  type        = string
+  default     = "standard-v3"
+}
+
+variable "core_fraction" {
+  description = "Core fraction for the virtual machine"
+  type        = number
+  default     = 20
+}
+
+variable "cores" {
+  description = "Number of cores for the virtual machine"
+  type        = number
+  default     = 2
+}
+
+variable "memory" {
+  description = "Amount of memory (in GB) for the virtual machine"
+  type        = number
+  default     = 2
+}
+
+variable "metadata-path" {
+  type    = string
+  default = "./meta.yml"
+}
+
+variable "allow_stopping_for_update" {
+  type        = bool
+  default     = true
+  description = "Is stopping for updates allowed"
+}
+
+# NAT
+
+variable "vm_nat_name" {
+  type        = string
+  default     = "nat-vm"
+  description = "NAT instance name"
+}
+
+variable "nat_ip_address" {
+  type        = string
+  default     = "192.168.10.254"
+  description = "IP address of NAT instance"
+}
+
+# Boot Disk for NAT
+
+variable "nat_boot_disk_name" {
+  type        = string
+  default     = "boot-disk-nat"
+  description = "Name of the boot disk for NAT instance"
+}
+
+variable "nat_boot_disk_image_id" {
+  type        = string
+  default     = "fd80mrhj8fl2oe87o4e1"
+  description = "ID of the image to create NAT boot disk"
+}
+
+# Boot Disk for other VM
+
+variable "vm_boot_disk_name" {
+  type        = string
+  default     = "boot-disk"
+  description = "Name of the boot disk for NAT instance"
+}
+
+variable "vm_boot_disk_image_id" {
+  type        = string
+  default     = "fd8ludhmmor1p0c3q6k0"
+  description = "ID of the image to create public vm boot disk"
+}
+
+# Boot Disk common
+
+variable "boot_disk_type" {
+  type        = string
+  default     = "network-hdd"
+  description = "Type of the boot disk for NAT instance"
+}
+
+variable "boot_disk_size" {
+  type        = number
+  default     = 10
+  description = "Size of the boot disk (in GB)"
+}
+
+# Security Group
+
+variable "sg_nat_name" {
+  type        = string
+  description = "The name of the NAT instance security group"
+  default     = "nat-instance-security-group"
+}
+
+# Public VM
+
+variable "public_vm_name" {
+  type        = string
+  default     = "public-vm"
+  description = "Public instance name"
 }
