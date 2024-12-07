69eb79840c451e8c633e8054e69cc100c13d320c
Pesheva Irina <pesheva.i@yandex.ru>
2024-12-07 13:21:08 +0300

    Prepare files for 23.4

--- homework/23.common/buckets.tf
+++ homework/23.common/buckets.tf
@@ -1,13 +1,13 @@
-# resource "yandex_storage_bucket" "image-bucket" {
-#   access_key            = yandex_iam_service_account_static_access_key.sa-static-key.access_key
-#   secret_key            = yandex_iam_service_account_static_access_key.sa-static-key.secret_key
-#   bucket                = var.bucket_name
-#   max_size              = var.bucket_max_size
+resource "yandex_storage_bucket" "image-bucket" {
+  access_key            = yandex_iam_service_account_static_access_key.sa-static-key.access_key
+  secret_key            = yandex_iam_service_account_static_access_key.sa-static-key.secret_key
+  bucket                = var.bucket_name
+  max_size              = var.bucket_max_size
 
-#   anonymous_access_flags {
-#     read        = var.is_readable
-#   }
-# }
+  anonymous_access_flags {
+    read        = var.is_readable
+  }
+}
 
 resource "yandex_storage_bucket" "kms-bucket" {
   access_key            = yandex_iam_service_account_static_access_key.sa-static-key.access_key
